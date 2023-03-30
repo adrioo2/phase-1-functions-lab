9d69d739fe4443991ef0f4ff896c99a246434536
Lourdes Quizhpilema <adrianalou42@gmail.com>
2023-03-30 18:25:03 -0400

    pass

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (location) {
+    let headQuarter = 42
+  if (location <= 42) {
+    return headQuarter - location
+  }
+else return location - headQuarter
+}
+
+function distanceFromHqInFeet(number) {
+    return distanceFromHqInBlocks(number) * 264}
+
+function distanceTravelledInFeet(start, destination) {
+    let total = start - destination
+    let totalTwo = destination - start
+    if (destination < start) {
+        return total * 264}
+    else return totalTwo * 264
+        }
+
+        function distanceFromHqInBlocks (location) {
+            let headQuarter = 42
+          if (location <= 42) {
+            return headQuarter - location
+          }
+        else return location - headQuarter
+        }
+        
+        function distanceFromHqInFeet(number) {
+          return distanceFromHqInBlocks(number) * 264}
+        
+        function distanceTravelledInFeet(start, destination) {
+              let total = start - destination
+          let totalTwo = destination - start
+          if (destination < start) {
+              return total * 264}
+          else return totalTwo * 264
+              }
+        function calculatesFarePrice(start,destination) {
+          let  distanceOne = distanceTravelledInFeet(start, destination)
+          let distanceTwo = 0
+          if (distanceOne <= 400) {
+            return distanceTwo}
+          else if (distanceOne > 400 && distanceOne <=2000) {
+            return (distanceOne - 400) * 0.02
+        }
+          else if (distanceOne > 2000 && distanceOne <=2500) {
+            return 25}
+          else {return 'cannot travel that far'}
+          }
+          
+            
